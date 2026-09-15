0197919c5428d6cc44caf94f70d6012b6cf4b186
tonophoto <tonops@outlook.jp>
2026-09-15 12:57:15 +0900

    chore: bust photography sync cache

--- photography-sounds.js
+++ photography-sounds.js
@@ -142,6 +142,6 @@
 
 if (document.documentElement.dataset.authPage === 'photography') {
   const syncScript = document.createElement('script');
-  syncScript.src = './photography-sync.js?v=20260915-2';
+  syncScript.src = './photography-sync.js?v=20260915-3';
   document.head.appendChild(syncScript);
 }
